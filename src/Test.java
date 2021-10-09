1bf2e68eafb780276925df45f943cd3ada4f7474
Andrii <dryunya093@gmail.com>
2021-10-10 00:43:31 +0300

    This is a combination of 2 commits.
    Resolved conflict
    
    Test.java added to feature_branch with change on string #2

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,17 @@
 public class Test {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Java");
+=======
+        System.out.printLn("education");
+<<<<<<< HEAD
+>>>>>>> 4446a04 (test.java added to feature_branch)
+=======
+        //new string
+<<<<<<< HEAD
+>>>>>>> 3349ccd (Test.java added to feature_branch with change on string #2)
+=======
+        System.out.println("DICT")
+>>>>>>> 7f678ad (Test.java added to feature_branch with change on string#3)
     }
 }
