8d6420a7cfe13e5850acf6fff7c16c3027c155f2
Andrii <dryunya093@gmail.com>
2021-12-19 19:11:21 +0200

    Tictactoe 2 stage

--- src/Tictactoe/Tictactoe.java
+++ src/Tictactoe/Tictactoe.java
@@ -1,9 +1,20 @@
 package Tictactoe;
 
+import java.util.Scanner;
+
 public class Tictactoe {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+
+        System.out.println("---------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("%s ", line.charAt(3 * i + j));
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
     }
 }
