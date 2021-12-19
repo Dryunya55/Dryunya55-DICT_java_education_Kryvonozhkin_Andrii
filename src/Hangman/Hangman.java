97901ad362568f460687bb316a6ed31e4639e189
Andrii <dryunya093@gmail.com>
2021-12-19 03:34:05 +0200

    CoffeeMachine 1 stage

--- src/Hangman/Hangman.java
+++
@@ -1,8 +0,0 @@
-package Hangman;
-
-public class Hangman {
-    public static void main(String[] args) {
-        System.out.println("HANGMAN" +
-                "The game will be available soon");
-    }
-}
