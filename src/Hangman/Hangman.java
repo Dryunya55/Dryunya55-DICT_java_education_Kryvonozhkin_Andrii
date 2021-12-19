b4c16aa50eca4d42aefeaf5b2252c625b168aae9
Andrii <dryunya093@gmail.com>
2021-12-19 02:43:31 +0200

    Hangman 1 stage

---
+++ src/Hangman/Hangman.java
@@ -0,0 +1,8 @@
+package Hangman;
+
+public class Hangman {
+    public static void main(String[] args) {
+        System.out.println("HANGMAN" +
+                "The game will be available soon");
+    }
+}
