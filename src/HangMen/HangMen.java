86f02990c5cc2b3af6d581c255fee86503313c12
Andrii <dryunya093@gmail.com>
2021-12-22 23:14:34 +0200

    HangMen 4 stage

--- src/HangMen/HangMen.java
+++ src/HangMen/HangMen.java
@@ -1,37 +1,33 @@
 package HangMen;
 
-import java.util.Objects;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
-
-
 public class HangMen {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        StringBuffer buffer = new StringBuffer("");
         System.out.println("HANGMAN");
-        String user = input.nextLine();
-        if (Objects.equals(user, "java")) {
-            System.out.println("You survived!");
-        } else {
-            System.out.println("You lost!");
-            ArrayList<String> lst = new ArrayList<>();
-            lst.add("python");
-            lst.add("java");
-            lst.add("javascript");
-            lst.add("kotlin");
+        ArrayList<String> lst = new ArrayList<>();
+        lst.add("python");
+        lst.add("java");
+        lst.add("javascript");
+        lst.add("kotlin");
 
-            String word = lst.get(new Random().nextInt(lst.size()));
-            int repeat = 0;
-            while (repeat != 8) {
-                String users = input.nextLine();
-                if (word.equals(users)) {
-                    System.out.println("You survived!");
-                    break;
-                } else {
-                    System.out.println("You lost!");
-                    repeat++;
-                }
+        String word = lst.get(new Random().nextInt(lst.size()));
+        int repeat = 0;
+        buffer.append(word);
+        StringBuffer help = buffer.delete(2, buffer.length());
+        for (int i = 1; i <= word.length() - 2; i++) help.append("-");
+        while (repeat != 8) {
+            System.out.println("Guess the word" + " " + help + ":");
+            String user = input.nextLine();
+            if (word.equals(user)) {
+                System.out.println("You survived!");
+                break;
+            } else {
+                System.out.println("You lost!");
+                repeat++;
             }
         }
     }
